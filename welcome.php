1b49c3bc15277cf253dd54f38a7393b341429a10
Zahra Mahmood <zahramahmood16@gmail.com>
2014-08-11 13:10:24 -0400

    New Website Layout

---
+++ welcome.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>McNair Alumni Association</title>
+		<link rel="stylesheet" type="text/css" href="welcome.css">
+	<head>
+	<body>
+		<div class="header">
+			<div id="logo"><img src = "./images/icons/McNairHSlogo.png"></div>
+			<div id="social-connections">
+				<div id="facebook"></div>
+				<div id="twitter"></div>
+				<div id="linkedin"></div>
+			</div>
+		</div>
+		<div id="Macro">
+			<div class="header">
+				<div id="link-wrapper">
+					<div id="link1"><a>Link 1</a></div>
+					<div id="link2"><a>Link 2</a></div>
+					<div id="link3"><a>Link 3</a></div>
+				</div>
+			</div>
+			<div id="title">
+				<a> Dr. Ronald E. McNair Academic High School</a>
+			</div>
+		</div>
+
+	</body>
+</html>
